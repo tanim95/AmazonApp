162fc3cf006c15cc519e468bcf5d40e8223e70ab
tanim95 <tanimrahman78@gmail.com>
2021-07-08 08:44:26 +0600

    navbar done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCGc6uDgp7ApWB3r_J9gM3LCnqMZnXmI5U',
+  authDomain: 'clone-7cab5.firebaseapp.com',
+  databaseURL: 'https://clone-7cab5-default-rtdb.firebaseio.com',
+  projectId: 'clone-7cab5',
+  storageBucket: 'clone-7cab5.appspot.com',
+  messagingSenderId: '364274573797',
+  appId: '1:364274573797:web:e6a6ded667bfb23d9f8f0c',
+  measurementId: 'G-P9BZM1CHCT',
+};
