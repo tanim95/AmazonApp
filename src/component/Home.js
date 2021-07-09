e1189d517be171e006897f0c67480c0201c8951d
tanim95 <tanimrahman78@gmail.com>
2021-07-09 18:16:39 +0600

    product component styling is ready

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,14 +6,20 @@ import Product from './Product';
 function Home() {
   return (
     <div className='home'>
+      <img className='imge' src={amazon} alt='' />
       <div className='home-container'>
-        <img className='imge' src={amazon} alt='' />
-
+        <div className='home__row'>
+          <Product />
+          <Product />
+        </div>
+        <div className='home__row'>
+          <Product />
+          <Product />
+          <Product />
+        </div>
         <div className='home__row'>
           <Product />
         </div>
-        <div className='home__row'></div>
-        <div className='home__row'></div>
       </div>
     </div>
   );
