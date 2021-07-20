9c916498fa55f934c5a5e00bee5fe8a088be4660
tanim95 <tanimrahman78@gmail.com>
2021-07-20 13:59:01 +0600

    commited

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,8 +2,16 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import logo from '../Images/AmazonLogo.png';
 import './Login.css';
+import { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='loggin'>
       <Link to='/'>
@@ -13,10 +21,20 @@ const Login = () => {
         <h1>Sign In</h1>
         <form>
           <h5>E-mail</h5>
-          <input type='text' />
+          <input
+            type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type='password' />
-          <button className='login__signin'>Sign in</button>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type='submit' onClick={signIn} className='login__signin'>
+            Sign in
+          </button>
         </form>
         <p>By sigining in you are agree to amazo's terms and condition</p>
 
